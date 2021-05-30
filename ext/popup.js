b242f71b09be8b042d370a5a6e3453842afd1230
asyraffff <amirulasyraf4.0x@gmail.com>
2021-05-30 17:18:01 +0800

    Add: Icon Changes

--- ext/popup.js
+++ ext/popup.js
@@ -19,15 +19,44 @@ function restoreOptions() {
 
 function saveOptions() {
   var blockedSites = document.getElementById('blocked-sites').value;
-  chrome.storage.sync.set({
-    blockedSites: blockedSites
-  }, function() {
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 2500);
-  });
+
+  if (blockedSites == "") {
+    chrome.storage.sync.set({
+      blockedSites: blockedSites
+    }, function() {
+      // chrome.browserAction.setBadgeText({
+      //   text: 'OFF'
+      // });
+
+      chrome.browserAction.setIcon({
+                path: {
+                    19: "icons/not_active_icon.png"
+                }
+            });
+    });
+
+  } else {
+    chrome.storage.sync.set({
+      blockedSites: blockedSites
+    }, function() {
+      var status = document.getElementById('status');
+
+      // chrome.browserAction.setBadgeText({
+      //   text: 'ON'
+      // });
+
+      chrome.browserAction.setIcon({
+                  path: {
+                      19: "icons/active_icon.png"
+                  }
+              });
+
+      status.textContent = 'Options saved.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 2500);
+    });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
